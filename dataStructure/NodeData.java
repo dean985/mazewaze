f8720ecf8497c980334bc2c1c65de157421accc8
Elon Ezra <elon-kadatz@outlook.com>
2019-12-24 09:09:51 +0200

    Create NodeData.java

---
+++ dataStructure/NodeData.java
@@ -0,0 +1,71 @@
+
+import java.util.Hashtable;
+
+public class NodeData implements node_data {
+    int key;
+    Point3D position;
+    double weight;
+    int tag;
+    Hashtable<NodeData,Integer > connections =
+            new Hashtable<NodeData,Integer>();
+
+
+   public NodeData(int key, Point3D point3D)
+    {
+        this.key = key;
+        this.position = point3D;
+    }
+
+    @Override
+    public int getKey() {
+        return this.key;
+    }
+
+    @Override
+    public Point3D getLocation() {
+        return position;
+    }
+
+    @Override
+    public void setLocation(Point3D p) {
+        position = p;
+    }
+
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public void setWeight(double w) {
+        weight = w;
+    }
+
+    @Override
+    public String getInfo() {
+       String str ;
+       str = "key: " +  Double.toString(key) + " ";
+       str += "(" + position.x() + "," + position.y() + "," + position.z() + ") \n";
+       str += connections.toString();
+        return str;
+    }
+
+    @Override
+    public void setInfo(String s)
+    {
+
+    }
+
+    @Override
+    public int getTag() {
+        return tag;
+    }
+
+    @Override
+    public void setTag(int t) {
+
+       tag = t;
+    }
+
+
+}
